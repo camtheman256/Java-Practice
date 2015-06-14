38e72457b100ddc9f0d1de0113a66b291f5a8aaf
Cameron Kleiman <camtheman256@gmail.com>
2015-06-14 15:54:22 -0500

    Uploaded current state of Problem13 and deleted class files

---
+++ problem13.java
@@ -0,0 +1,10 @@
+class Problem13 {
+  public static void main(String[] args) {
+    int addition = 0;
+    int[] numbers = {}
+    for(int i = 0; i < numbers.length; i++) {
+      
+    }
+    System.out.prinln(addition);
+  }
+}
